10a50050c22fbbaded2aa5f73c5e4ed593098ea4
11PRIMUS <alokpal2803@gmail.com>
2025-10-07 07:11:25 +0530

    nn

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from config import DATA_DIR
+from agents.base_agent import Baseagent
+
 def main():
     print("ARC-Agentic Reasoning Code")
+    print(f"Data dir : {DATA_DIR}")
+
+    try:
+        print("BaseAgent class loaded")
+    except Exception as e:
+        print(f"error: {e}")
+
 
 if __name__=="__main__":
     main()
